806357ba6628d3f2d14df122b27edc74e62cfa1f
Chandra Mouli Katta <110664908+Chandramouli2714@users.noreply.github.com>
2024-02-22 09:10:42 +0530

    Add files via upload

---
+++ Day-1/relation.py
@@ -0,0 +1,13 @@
+a,b = map(int,input().split())
+
+if a>b:
+    print("a > b")
+elif b>a:
+    print("b > a")
+elif a<b:
+    print("a < b")
+elif b<a:
+    print("b < a")
+else:
+    print("a == b")
+    
