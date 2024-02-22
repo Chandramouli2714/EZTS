32fa081ff726fc72a99b3e591f42d1d569215ded
Chandra Mouli Katta <110664908+Chandramouli2714@users.noreply.github.com>
2024-02-22 09:20:13 +0530

    Delete Day-1 directory

--- Day-1/compare.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter the value a: "))
-b = int(input("Enter the value b: "))
-if a>b:
-    print("The largest value is :",a)
-else:
-    print("The largest value is :",b)
--- Day-1/discount.py
+++
@@ -1,8 +0,0 @@
-test_case = int(input())
-'''while test_case > 0:
-    n = int(input())
-    print(100-n)
-    test_case = test_case - 1'''
-for i in range(test_case):
-    n = int(input())
-    print(100-n)
